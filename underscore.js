00a530630c58240bb8eaf521a9043823634b4903
TonyNguyen101 <anthonylenguyen04@gmail.com>
2015-05-04 22:52:10 -0700

    added 2nd part

--- underscore.js
+++ underscore.js
@@ -1,1 +1,40 @@
-// PUT YOUR CODE HERE!!!
+// Part 2
+
+
+var _ = (function() {
+	return {
+		each : function(array, alert) {
+			for (var i = 0; i < array.length; i++) {
+			alert(array[i]);
+			}
+		},
+
+ 		compact : function(array) {
+			var newArray = [];
+			for (var i = 0; i < array.length; i++) {
+				if (array[i] !== undefined) {
+					newArray.push(array[i]);
+				}
+			}
+			return newArray;
+		},
+
+		map : function(array, action) {
+			var newArray = [];
+			for (var i = 0; i < array.length; i++) {
+				newArray.push(action(array[i]));
+			}
+			return newArray; 
+		},
+
+		filter : function(array, action) {
+			var newArray = [];
+			for (var i = 0; i < array.length; i++) {
+				if (action(array[i]) === true) {
+					newArray.push(array[i]);
+				}
+			}
+			return newArray;
+		}
+	};
+})();
